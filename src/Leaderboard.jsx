ab58f2d86ed39cd317561ba7c99e95d0d14886ac
spikest3r <oleg0sher@outlook.com>
2025-09-05 22:58:44 +0300

    fixed table centering

--- src/Leaderboard.jsx
+++ src/Leaderboard.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { useTranslation } from "react-i18next";
+import './Leaderboard.css'
 
 function Leaderboard() {
   const [leaderboardData, setLeaderboardData] = useState([]);
